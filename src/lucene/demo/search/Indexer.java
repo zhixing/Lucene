cc1f7b925576388e571e02d090a614d476a5cae9
jasonpeng <jason@r-235-177-28-172.comp.nus.edu.sg>
2013-09-19 17:45:39 +0800

    updated indexer

--- src/lucene/demo/search/Indexer.java
+++ src/lucene/demo/search/Indexer.java
@@ -11,96 +11,98 @@ package lucene.demo.search;
 
 import java.io.File;
 import java.io.IOException;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Document;
-import lucene.demo.business.Hotel;
-import lucene.demo.business.HotelDatabase;
+import java.util.List;
+
+import lucene.demo.business.RawDocument;
+import lucene.demo.business.RawDocumentDatabase;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
-
 public class Indexer {
-    boolean isIndexedAlready;
-	
-    /** Creates a new instance of Indexer */
-    public Indexer() {
-    }
- 
-    private IndexWriter indexWriter = null;
-    
-    public IndexWriter getIndexWriter(boolean create) throws IOException {
-        if (indexWriter == null&&create) {
-            
-    		final File docDir = new File("index-directory");
+	boolean isIndexedAlready;
 
-            if (docDir.exists()) {
-                System.out.println("Index at '" + docDir.getAbsolutePath() + "' is already built (delete first!)");
-                isIndexedAlready = true;
-                return null;
-                //System.exit(1);
-            } else {
-            	isIndexedAlready = false;
-            }
-        	
-        	FSDirectory idx = FSDirectory.open(new File("index-directory"));
-    		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(
-    				Version.LUCENE_CURRENT, new StandardAnalyzer(Version.LUCENE_CURRENT));
+	/** Creates a new instance of Indexer */
+	public Indexer() {
+	}
 
-    		 indexWriter = new IndexWriter(idx, indexWriterConfig);
-            
-        }
-        return indexWriter;
-   }    
-   
-    public void closeIndexWriter() throws IOException {
-        if (indexWriter != null) {
-            indexWriter.close();
-        }
-   }
-    
-    public void indexHotel(Hotel hotel) throws IOException {
+	private IndexWriter indexWriter = null;
 
-        indexWriter = getIndexWriter(false);
-        
-        if (isIndexedAlready){
-        	return;
-        } else{
-        }
-        
-        System.out.println("Indexing hotel: " + hotel);
-        
-        Document doc = new Document();
-        doc.add(new Field("id", hotel.getId(), Field.Store.YES, Field.Index.NO));
-        doc.add(new Field("name", hotel.getName(), Field.Store.YES, Field.Index.ANALYZED));
-        doc.add(new Field("city", hotel.getCity(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-        doc.add(new Field("description", hotel.getDescription(), Field.Store.YES, Field.Index.ANALYZED));
-        String fullSearchableText = hotel.getName() + " " + hotel.getCity() + " " + hotel.getDescription();
-        doc.add(new Field("content", fullSearchableText, Field.Store.NO, Field.Index.ANALYZED));
-        indexWriter.addDocument(doc);
-    }   
-    
-    public void rebuildIndexes() throws IOException {
-          //
-          // Erase existing index
-          //
-          getIndexWriter(true);
-          //
-          // Index all Accommodation entries
-          //
-          Hotel[] hotels = HotelDatabase.getHotels();
-          for(Hotel hotel : hotels) {
-              indexHotel(hotel);              
-          }
-          //
-          // Don't forget to close the index writer when done
-          //
-          closeIndexWriter();
-     }    
-    
-  
-    
-    
+	public IndexWriter getIndexWriter(boolean create) throws IOException {
+		if (indexWriter == null && create) {
+
+			final File docDir = new File("index-directory");
+
+			if (docDir.exists()) {
+				System.out.println("Index at '" + docDir.getAbsolutePath()
+						+ "' is already built (delete first!)");
+				isIndexedAlready = true;
+				return null;
+			} else {
+				isIndexedAlready = false;
+			}
+
+			FSDirectory idx = FSDirectory.open(new File("index-directory"));
+			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(
+					Version.LUCENE_36, new StandardAnalyzer(
+							Version.LUCENE_36));
+
+			indexWriter = new IndexWriter(idx, indexWriterConfig);
+
+		}
+		return indexWriter;
+	}
+
+	public void closeIndexWriter() throws IOException {
+		if (indexWriter != null) {
+			indexWriter.close();
+		}
+	}
+
+	public void indexRawDocument(RawDocument rawDocument) throws IOException {
+
+		indexWriter = getIndexWriter(false);
+
+		if (isIndexedAlready) {
+			return;
+		} else {
+		}
+
+		System.out.println("Indexing raw document: " + rawDocument.getId());
+
+		Document doc = new Document();
+		doc.add(new Field("id", rawDocument.getId(), Field.Store.YES,
+				Field.Index.NO));
+		doc.add(new Field("title", rawDocument.getTitle(), Field.Store.YES,
+				Field.Index.ANALYZED));
+		doc.add(new Field("text", rawDocument.getText(), Field.Store.YES,
+				Field.Index.ANALYZED));
+		doc.add(new Field("author", rawDocument.getAuthor(), Field.Store.YES,
+				Field.Index.ANALYZED));
+		doc.add(new Field("others", rawDocument.getOthers(), Field.Store.YES,
+				Field.Index.ANALYZED));
+
+		String fullSearchableText = rawDocument.getTitle() + " "
+				+ rawDocument.getText() + " " + rawDocument.getAuthor();
+		doc.add(new Field("content", fullSearchableText, Field.Store.NO,
+				Field.Index.ANALYZED));
+		indexWriter.addDocument(doc);
+	}
+
+	public void rebuildIndexes() throws IOException {
+		getIndexWriter(true);
+
+		List<RawDocument> rawDocuments = RawDocumentDatabase.getRawDocuments();
+		for (RawDocument rawDocument : rawDocuments) {
+			indexRawDocument(rawDocument);
+		}
+
+		closeIndexWriter();
+	}
+
 }
