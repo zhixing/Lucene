6c4f59d2100a378d9a34f29e68dfeb378a270f09
zhixing <zhixing135@hotmail.com>
2013-09-19 16:34:34 +0800

    CommandLine UI done

--- src/lucene/demo/Main.java
+++ src/lucene/demo/Main.java
@@ -7,6 +7,13 @@
 
 package lucene.demo;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import lucene.demo.search.*;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.ScoreDoc;
@@ -15,6 +22,25 @@ public class Main {
 
 	/** Creates a new instance of Main */
 	public Main() {
+	}
+	
+	private static void performNewSearch(String query) throws Exception{
+		System.out.println("performSearch");
+		SearchEngine instance = new SearchEngine();
+		ScoreDoc[] hits = instance.performSearch(query, 10);
+
+		System.out.println("Results found: " + hits.length);
+		for (int i = 0; i < hits.length; i++) {
+			ScoreDoc hit = hits[i];
+			Document doc = instance.searcher.doc(hits[i].doc); // This
+																// retrieves
+																// the
+
+			System.out.println(i + " " + doc.get("name") + " " + doc.get("city")
+					+ " (" + hit.score + ")");
+
+		}
+		System.out.println("performSearch done");
 	}
 
 	/**
@@ -29,26 +55,49 @@ public class Main {
 			Indexer indexer = new Indexer();
 			indexer.rebuildIndexes();
 			System.out.println("rebuildIndexes done");
-
-			// perform search on "Dame museum"
+			
+			// Let user enter query
+			InputStreamReader converter = new InputStreamReader(System.in);
+			BufferedReader in = new BufferedReader(converter);
+			System.out.println("Please enter your query:");
+			String userQuery = in.readLine();
+			
 			// and retrieve the result
-			System.out.println("performSearch");
-			SearchEngine instance = new SearchEngine();
-			ScoreDoc[] hits = instance.performSearch("Dame museum", 10);
-
-			System.out.println("Results found: " + hits.length);
-			for (int i = 0; i < hits.length; i++) {
-				ScoreDoc hit = hits[i];
-				// Document doc = hit.doc();
-				Document doc = instance.searcher.doc(hits[i].doc); // This
-																	// retrieves
-																	// the
-
-				System.out.println(doc.get("name") + " " + doc.get("city")
-						+ " (" + hit.score + ")");
-
+			try{
+				performNewSearch(userQuery);
+				
+				System.out.println("Enter indexes of relevent results, seperated by space. Press enter to proceed.");
+				String currentLine = in.readLine();
+				while(!currentLine.equals("")){
+					
+					// See what's relevent:
+					String[] splited = currentLine.split(" ");
+					int[] index = new int[splited.length];
+					for (int i = 0; i < splited.length; i++) {
+					    index[i] = Character.getNumericValue(splited[i].charAt(0));
+					}
+					
+					System.out.print("You have chosen indexes: "); 
+					for(int i : index){
+						System.out.print(" " + i);
+					}
+					System.out.println("");
+					
+					// Optimize the query. And search again:
+					ArrayList<String> releventList = null;
+					ArrayList<String> irreleventList = null;
+					userQuery = queryOptimizer.performRelevenceFeedback(userQuery, releventList, irreleventList);
+					System.out.println("Searching again based on your feedback, and the query is:");
+					System.out.println(userQuery);
+					performNewSearch(userQuery);
+					
+					System.out.println("Enter indexed of relevent results, seperated by space. Press enter to proceed.");
+					currentLine = in.readLine();				
+				}
+			} catch(IOException e){
+				System.out.println(e.getMessage());
 			}
-			System.out.println("performSearch done");
+
 		} catch (Exception e) {
 			System.out.println("Exception caught.\n");
 			System.out.println(e.toString());
